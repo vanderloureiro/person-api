5b9c06795ae4ba0e01518769b9fbdd09b2b3f075
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-10-26 13:40:12 -0300

    criacao de repository via map

---
+++ src/main/java/dev/vanderloureiro/person_api/person/PersonRepository.java
@@ -0,0 +1,40 @@
+package dev.vanderloureiro.person_api.person;
+
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+/*
+* Repository com visibildiade package para ser acessado apenas dentro do modulo de person,
+* Demais operações na base devem ser pelos serviços
+* */
+@Repository
+class PersonRepository {
+
+    private final HashMap<Long, Person> database = new HashMap<Long, Person>();
+
+    /*
+    * Construtor privado para deixar a inicialização sob responsabilidade da pŕopria classe e do Spring
+    * */
+    private PersonRepository() {
+        this.fillBase();
+    }
+
+    private void fillBase() {
+        database.put(1L, new Person(1L, "Vanderlei", LocalDate.of(1997, 3, 29),
+                LocalDate.of(2024, 10, 1)));
+        database.put(2L, new Person(2L, "Wilker", LocalDate.of(1990, 5, 20),
+                LocalDate.of(2021, 1, 15)));
+        database.put(3L, new Person(3L, "Lucas", LocalDate.of(1999, 10, 10),
+                LocalDate.of(2022, 8, 12)));
+    }
+
+    // uso de fail first
+    public Person findById(Long id) {
+        if (!database.containsKey(id)) {
+            throw new RuntimeException();
+        }
+        return database.get(id);
+    }
+}
