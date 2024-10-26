1c6e1baae1ddcd5c7edfc152ba068b1dda60d5db
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-10-26 13:06:15 -0300

    inicio de projeto com dependencias

---
+++ src/main/java/dev/vanderloureiro/person_api/PersonApiApplication.java
@@ -0,0 +1,13 @@
+package dev.vanderloureiro.person_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiApplication.class, args);
+	}
+
+}
