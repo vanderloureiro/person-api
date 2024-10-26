39d24470707de5bbc6976f940395169bda0427fe
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-10-26 13:20:05 -0300

    classe POJO sem lombok

---
+++ src/main/java/dev/vanderloureiro/person_api/person/Person.java
@@ -0,0 +1,67 @@
+package dev.vanderloureiro.person_api.person;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private LocalDate birthDate;
+    private LocalDate admissionDate;
+
+    public Person(Long id, String name, LocalDate birthDate, LocalDate admissionDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.admissionDate = admissionDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(LocalDate admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id)
+                && Objects.equals(name, person.name)
+                && Objects.equals(birthDate, person.birthDate)
+                && Objects.equals(admissionDate, person.admissionDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, birthDate, admissionDate);
+    }
+}
